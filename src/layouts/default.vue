53e670f113760739c42156a07c35a256c5f64b24
曹亦鸣 <yiming.cao@palmaplus.com>
2025-11-13 16:22:12 +0800

    fix(layout): 🐛 修复页面标题读取问题，使用meta.title替代meta.menu.title

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,8 +4,8 @@ const head = useHead()
 
 watchEffect(() => {
   head.patch({
-    title: router.currentRoute.value.meta?.menu?.title
-      ? `${router.currentRoute.value.meta.menu.title} | ${import.meta.env.VITE_APP_NAME}`
+    title: router.currentRoute.value.meta?.title
+      ? `${router.currentRoute.value.meta.title} | ${import.meta.env.VITE_APP_NAME}`
       : import.meta.env.VITE_APP_NAME,
   })
 })
